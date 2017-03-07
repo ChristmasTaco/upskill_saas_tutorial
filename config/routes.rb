557735cee33a555c2bd307ede0d7110205e02c2d
Chris Losacco <chrislosacco13@gmail.com>
2017-03-07 23:51:09 +0000

    Added form fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
+    
+    # Make contacts more modular rather than having to write a line for every permutation, ie get 'contacts/new'
     resources :contacts
 end
