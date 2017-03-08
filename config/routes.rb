c55a63c358114466d5d511ba5392abd3ab1f9616
Chris Losacco <chrislosacco13@gmail.com>
2017-03-08 19:28:44 +0000

    Saving additional files for contacts table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     
-    # Make contacts shorter rather than having to write a line for every permutation, ie get 'contacts/new'
+    # Creates all CRUD routes for Contacts rather than having to write a line for every permutation, ie get 'contacts/new'
     # Also offers a bunch of routes that can be seen using $ rails routes
     resources :contacts
 end
