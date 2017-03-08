def599b7ef0a6f0e7655ac63a23ab4ba38fd1446
Chris Losacco <chrislosacco13@gmail.com>
2017-03-08 18:50:59 +0000

    Added contacts link in nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     
-    # Make contacts more modular rather than having to write a line for every permutation, ie get 'contacts/new'
+    # Make contacts shorter rather than having to write a line for every permutation, ie get 'contacts/new'
+    # Also offers a bunch of routes that can be seen using $ rails routes
     resources :contacts
 end
