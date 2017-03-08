931d354e6198ecf29727792078a02def0cb4fef9
Chris Losacco <chrislosacco13@gmail.com>
2017-03-08 19:26:48 +0000

    Created contacts table

---
+++ db/migrate/20170308191440_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contacts do |t|
+      # Each of these are columns in our DB table
+      t.string :name
+      t.string :email
+      t.text :comments
+      t.timestamps
+    end
+  end
+end
