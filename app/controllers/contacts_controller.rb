c8021dc4db8c00c7e210f915132a20a4e5c65230
Chris Losacco <chrislosacco13@gmail.com>
2017-03-09 16:27:00 +0000

    Added Hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,13 @@
 class ContactsController < ApplicationController
     def new
-        # Creating a new instance variable for each user's information
+        # Creating a new instance variable for each user's information in the system's memory, to be used later
+        # The form needs to identify all the fields as an object even before it has to use them - preparing a blank object
         @contact = Contact.new
     end
     
     def create
-        # Creating a new instance variable for each user's information to be saved to the db
+        # updating the @contact instance variable with the entered parameters (each user's information) to be saved to the db
+        # 'contact_params' is a function that is called (outlined below) that says that we will securely save the entered data into the db
         @contact = Contact.new(contact_params)
         if @contact.save
             redirect_to new_contact_path, notice: "Form successfully submitted!"
@@ -15,7 +17,8 @@ class ContactsController < ApplicationController
     end
     
     private
-        # Creating a method to ensure that all of the required parameters are sent when hitting submit
+        # Creating a private method to ensure that all of the required parameters are sent when hitting submit
+        # This private method can only be used inside of this file
         def contact_params
             # Whitelisting all of the parameters
             params.require(:contact).permit(:name, :email, :comments) 
