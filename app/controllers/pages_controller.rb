b8f6731c5c39dbc0fe89544ec3fb0d564a85f42b
Chris Losacco <chrislosacco13@gmail.com>
2017-03-16 18:02:42 +0000

    Added parameters to sign-up links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+    # GET request for / (home page)
     def home
+        # Creating instance variables to be used as URL parameters when linking to signup view
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
     def about
