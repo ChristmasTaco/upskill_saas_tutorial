a49d2060639f4384d04b866413fa0982f64e6e0b
Chris Losacco <chrislosacco13@gmail.com>
2017-03-21 19:42:36 +0000

    Added user profile form and updated profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,19 @@
 class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
-    
+    @profile = Profile.new
   end
   
   # POST to /users/:user_id/profile
   def create
-    
+     @profile = Profile.new(profile_params)
   end
+  
+  private
+    # Creating a private method to ensure that all of the required parameters are sent when hitting submit
+    # This private method can only be used inside of this file
+    def profile_params
+      # Whitelisting all of the parameters
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
