9e98ab5042df7e3ff30d2739db01241ed8c44fae
Chris Losacco <chrislosacco13@gmail.com>
2017-03-21 20:05:27 +0000

    Added create action in Profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,14 +6,28 @@ class ProfilesController < ApplicationController
   
   # POST to /users/:user_id/profile
   def create
-     @profile = Profile.new(profile_params)
+    # Grab the user from the URL params
+    @user = User.find( params[ :user_id ] )
+     
+    # Associate the @profile variable with the user that is logged in per params
+    # build_profile is a new method
+    @profile = @user.build_profile( profile_params )
+    
+    if @profile.save
+      # Generate a flash message to let the user know the save was successful
+      flash[:success] = "Profile Updated!"
+      redirect_to root_path
+    else
+      # Re-render the form to allow the user to re-submit
+      render action: :new
+    end
   end
   
   private
     # Creating a private method to ensure that all of the required parameters are sent when hitting submit
     # This private method can only be used inside of this file
     def profile_params
-      # Whitelisting all of the parameters
+      # Whitelisting all of the parameters for security
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 end
