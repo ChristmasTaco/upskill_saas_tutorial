d61e79ca3340443cd7a169dec424edad4e8a2adc
Chris Losacco <chrislosacco13@gmail.com>
2017-03-21 16:19:51 +0000

    Added profiles controller, new view, and link on home view

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+  # GET to /users/:user_id/profile/new
+  def new
+    
+  end
+  
+  # POST to /users/:user_id/profile
+  def create
+    
+  end
+end
